5079f21a2bad0d1ff9102433d4b05c447203e679
Gokulraj08 <gokulraj08@gmail.com>
2026-08-26 23:15:59 +0530

    task1

---
+++ Task1.js
@@ -0,0 +1,78 @@
+// A variable stores a value/data.
+// var, let, const
+// var name = "John";
+// let name = "John";
+// const name = "John";
+// Declaration: Creating a variable.
+// Initialization: Giving a variable its first value.
+// Reassignment: Changing the value of a variable.
+// Redeclaration: Declaring the same variable again.
+// var
+// var and let
+// const
+// Declaration: let age | Initialization: = 25
+// 100
+// number = 200;
+// ---------var---------
+// 10
+// 20
+// 30
+// var name = "John";
+// var price = 500;
+// price = 1000;
+// 100
+// Yes.
+// Yes.
+// var x = 10; var x = 20;
+// -----------let-----------
+// let age = 25;
+// 30
+// Yes.
+// No.
+// SyntaxError: Identifier name has already been declared.
+// let city = "Chennai";
+// city = "Salem";
+// 50
+// let salary = 25000;
+// salary = 30000;
+// ------------const------------
+// const pi = 3.14;
+// No.
+// No.
+// const must be initialized when declared.
+// TypeError: Assignment to constant variable.
+// const country = "India";
+// 100
+// const
+// let can be reassigned; const cannot.
+// var allows redeclaration and reassignment; const allows neither and requires initialization.
+// ------------Printing & Console----------
+// console.log("Hello World");
+// console.log(500);
+// Displays a warning message.
+// Displays an error message.
+// alert()-Shows a message popup.
+// prompt()-Takes input from the user.
+// confirm()-Asks for OK/Cancel confirmation.
+// document.writeln() - Writes content to the webpage.
+// console.log()-Prints output in the console.
+// ------------Practical-------------
+// 1.let studentName = "Raj";
+// let age = 20;
+// let mark = 85;
+
+// console.log(studentName, age, mark);
+
+
+// 2.let name = prompt("Enter your name:");
+// alert(name);
+
+// 3.let age = prompt("Enter your age:");
+// console.log(age);
+
+
+// 4.confirm("Are you ready?");
+
+
+// 5.let name = prompt("Enter your name:");
+// document.writeln(name);
